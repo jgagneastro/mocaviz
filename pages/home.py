11956c5d137a52c469b9076045470dac713273fd
Jonathan Gagne <jonathan.gagne.1@gmail.com>
2023-03-17 11:06:44 -0400

    Moved xyz.css which broke the moca-explorer page and started propagating credentials

--- pages/home.py
+++ pages/home.py
@@ -1,26 +1,43 @@
 import dash
 from dash import html, dcc
+from dash.dependencies import Input, Output, State
 
 dash.register_page(__name__, path='/')
+
+default_text = \
+" Welcome to the MOCAdb data visualizations center." + \
+" This web page contains several tools that will allow you to interactively explore the contents of the MOCA database." + \
+" You can also access the Python codes used to run these visualizations" + \
+" [on my GitHub](https://github.com/jgagneastro/mocaviz) and run them locally.\n\n" + \
+" Please choose one of the following tools:\n" + \
+" * Explore general MOCAdb data with the [MOCA explorer](/moca-explorer#args#).\n" + \
+" * Explore the 3D XYZ spatial positions of MOCAdb data with the [MOCA spatial explorer](/xyz#args#).\n" + \
+" * Explore hierarchical association structures with a [Sunburst graph](/group-hierarchy#args#).\n" + \
+" * Return to the [MOCAdb website](https://mocadb.ca).\n"
 
 layout = html.Div(
     className="banner",
     children=[
+        dcc.Location(id='home-url', refresh=False),
         #html.Img(src=get_asset_url("dash-logo.png")),
         html.H6(children='MOCAdb data visualizations',style={'color':"#000000","backgroundColor":"#DEDDE1", "marginLeft": 0, "width":"100%"}),
         dcc.Markdown(
-                id="home-instructions",
-                className="row",
+                id='home-instructions',
+                className='row',
                 children=[
-                    " Welcome to the MOCAdb data visualizations center. This web page contains several tools that will allow you to interactively explore the contents of the MOCA database. You can also access the Python codes used to run these visualizations [on my GitHub](https://github.com/jgagneastro/mocaviz) and run them locally.\n\n"
-                    " Please choose one of the following tools:\n"
-                    " * Explore general MOCAdb data with the [MOCA explorer](/moca-explorer).",
-                    " * Explore the 3D XYZ spatial positions of MOCAdb data with the [MOCA spatial explorer](/xyz).",
-                    " * Explore hierarchical association structures with a [Sunburst graph](/group-hierarchy).",
-                    " * Return to the [MOCAdb website](https://mocadb.ca).",
+                    default_text
                 ]
                 , style={"width": "100%", "whiteSpace": "pre-wrap", "marginLeft": 30, "marginRight": 30, "marginTop": 30,},#, "backgroundColor":"#1F2C56"
             ),
         ],
     style={"width": "100%", "height": "100%", "whiteSpace": "pre-wrap", "backgroundColor":"#F5F8FA"},#, "backgroundColor":"#192444"
     )
+
+@dash.callback(output=Output('home-instructions', 'children'),
+    inputs=dict(
+        search=Input('home-url', 'search')
+    ),
+)
+def display_page(search):
+    return default_text.replace("#args#",search)
+    #return f"The current pathname is: {pathname}"
