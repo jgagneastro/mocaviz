fbcee0a2fb1676aa3a9951016b006af420798bf4
Jonathan Gagne <jonathan.gagne.1@gmail.com>
2026-05-13 15:58:02 -0400

    Tidy JavaScript pages landing link

--- pages/home.py
+++ pages/home.py
@@ -10,7 +10,7 @@ default_text = \
 " You can also access the Python codes used to run these visualizations" + \
 " [on my GitHub](https://github.com/jgagneastro/mocaviz) and run them locally.\n\n" + \
 " These are legacy pages based on Dash apps. Faster JavaScript pages are available at " + \
-" [dataviz.mocadb.ca/js](https://dataviz.mocadb.ca/js).\n\n" + \
+"[dataviz.mocadb.ca/js](https://dataviz.mocadb.ca/js).\n\n" + \
 " Please choose one of the following tools:\n" + \
 " * Explore the 3D XYZUVW spatial positions of MOCAdb data with the [MOCA spatial-kinematic explorer](/xyzuvw?#args#).\n" + \
 " * Explore hierarchical association structures with a [Sunburst graph](/group-hierarchy?#args#).\n" + \
